7e547cb077497bd2736fe43da6d8611dbd2f5daf
4NDROM3DA <115934587+4NDROM3DA@users.noreply.github.com>
2023-07-20 13:27:26 +0200

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+ TITLE stop services
+ECHO ---------------------------------------------------
+ECHO Simple batch script to stop services on Windows 11
+ECHO Services can be changed with a notepad
+ECHO ---------------------------------------------------
+ECHO SCRIPT MUST BE RUN AS ADMINISTRATOR
+ECHO ---------------------------------------------------
+
+:start
+SET choice=
+SET /p choice=Do you really want to continue? y/n: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='y' GOTO yes
+IF '%choice%'=='n' GOTO no
+IF '%choice%'=='' GOTO no
+ECHO "%choice%" is not valid. Type "y" or "n"
+ECHO.
+GOTO start
+
+:no
+ECHO Canceled
+PAUSE
+EXIT
+
+:yes
+net stop TimeBrokerSvc
+net stop SEMgrSvc
+net stop XblAuthManager
+net stop DoSvc
+net stop TrkWks
+ECHO Completed
+PAUSE
+EXIT
